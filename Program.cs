b93007845308fffb69730652f862d3b4c99d52e6
Kamalesh2703 <kamaleshg2703@gmail.com>
2022-11-29 11:59:02 +0530

    [Kamal]_hello wold

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Addition");
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+//Calucator
+//UC1-Addition
+//UC2-Sub
+//UC2-Div
